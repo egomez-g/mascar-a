569c68a20811e36eefb55d56c1b22c980179cbec
egomez-g <edu.gomez.gonzalezz@gmail.com>
2026-01-31 19:14:14 +0100

    shake

--- arrastrar.gd
+++ arrastrar.gd
@@ -6,24 +6,32 @@ var selected:bool = false
 var ini_pos:Vector2
 var true_pos:Vector2
 var pos_raton = Vector2(0, 0)
+var original_position: Vector2
+var basura_position: Vector2
+var boca_position: Vector2
+var fuerzaDelsake: int = 6
 signal OnTrash
 signal OnMouth
 
 @export var boca: Node2D
 @export var basura: Node2D
-
 @export var isSakeable: bool
 
 func _ready():
 	ini_pos = global_position
+	original_position = position
+	basura_position = basura.position
+	boca_position = boca.position
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if Input.is_action_just_pressed("click_izq"):
 		selected = true
 		ini_pos = global_position
+		isSakeable = false
 	if Input.is_action_just_released("click_izq"):
 		selected = false
 		check_if_boca()
+		isSakeable = true
 
 func _physics_process(delta: float) -> void:
 	if selected:
@@ -47,34 +55,11 @@ func check_if_boca():
 
 func _process(delta: float):
 	if isSakeable:
-		sakear()
-	else:
-		desSakear()
+		position = position.lerp(original_position + Vector2(randf_range(-fuerzaDelsake, fuerzaDelsake), randf_range(-fuerzaDelsake, fuerzaDelsake)), 0.4)
+	if selected:
+		sakear_lo_demas()
 
-func sakear():
-	brillo()
-
-func desSakear():
-	desbrillo()
-
-func brillo():
-	glow_tween = create_tween()
-	glow_tween.set_loops()
-	glow_tween.tween_property(
-		$Sprite2D,
-		"modulate",
-		Color(1.3, 1.3, 1.3),
-		0.15
-	).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-
-	glow_tween.tween_property(
-		$Sprite2D,
-		"modulate",
-		Color(1.0, 1.0, 1.0),
-		0.15
-	)
-
-func desbrillo():
-	if glow_tween:
-		glow_tween.kill()
-	$Sprite2D.modulate = Color(1, 1, 1)
+func sakear_lo_demas():
+	boca.position = boca.position.lerp(boca_position + Vector2(randf_range(-fuerzaDelsake, fuerzaDelsake), randf_range(-fuerzaDelsake, fuerzaDelsake)), 0.4)
+	basura.position = basura.position.lerp(basura_position + Vector2(randf_range(-fuerzaDelsake, fuerzaDelsake), randf_range(-fuerzaDelsake, fuerzaDelsake)), 0.4)
+	
