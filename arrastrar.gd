639dad17d9a1ff0b7a55fc1c966e45f62eab4738
egomez-g <edu.gomez.gonzalezz@gmail.com>
2026-01-31 12:26:21 +0100

    sin gayplay

--- arrastrar.gd
+++ arrastrar.gd
@@ -6,6 +6,9 @@ var ini_pos:Vector2
 var true_pos:Vector2
 var polla = 0
 var pos_raton = Vector2(0, 0)
+@onready var boca = get_node("../boca/pos_boca")
+@onready var basura = get_node("../basura/pos_basura")
+
 
 func _ready():
 	ini_pos = global_position
@@ -16,7 +19,7 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 		ini_pos = global_position
 	if Input.is_action_just_released("click_izq"):
 		selected = false
-		global_position = ini_pos
+		check_if_boca()
 
 func _physics_process(delta: float) -> void:
 	if selected:
@@ -25,3 +28,13 @@ func _physics_process(delta: float) -> void:
 	else:
 		global_position = lerp(global_position, ini_pos, 25 * delta)
 		rotation  = lerp_angle(rotation, 0, 5 * delta)
+
+func check_if_boca():
+	var dist_boca = get_global_mouse_position() - boca.global_position
+	var dist_basura = get_global_mouse_position() - basura.global_position
+	if dist_boca.x < 200 and dist_boca.y < 200:
+		print("boca")
+	elif dist_basura.x < 200 and dist_basura.y < 200:
+		print ("basura")
+	else:
+		global_position = ini_pos
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,11 +1,34 @@
 extends Node2D
 class_name main
 
-var enableMouse:bool = false
+var enableInput: bool = false
+var text_i: int = 0
 
+@onready var animacionDealer = get_node("./dealer/AnimationPlayer")
+@onready var animacionObjeto = get_node("./spriteObjeto/AnimationPlayer")
+@onready var shader = $shader2
+@onready var zonaTexto = $zonaTexto
+
+var textos := [
+	"DAME TODO LO QUE TENGAS JODEEER, ME ESTOY PONIENDO MUY NERVIOSO",
+	"E-Eh vale vale, tranquilo colega… N-no hagas ninguna tontería",
+	"¡¡¿¿ PERO QUE COJONES HACES ??!! ESTAS TOCADO DE LA CABEZA COLEGA, VAYA PUTA CIUDAD DE COLGADOS, ME PIRO DE AQUÍ",
+	"¿Pero…? Mira, déjalo…"
+]
 
 func _ready():
+	animacionDealer.play("appear")
+	animacionObjeto.play("appear")
 	_on_dealer_animation_finished("appear")
 
 func _on_dealer_animation_finished(anim_name: StringName) -> void:
-	enableMouse = true
+	enableInput = true
+
+func _input(event: InputEvent) -> void:
+	if enableInput:
+		if Input.is_action_just_pressed("tecla_a"):
+			if text_i < textos.size():
+				zonaTexto.text = textos[text_i]
+				text_i += 1
+			else:
+				shader.mouse_filter = Control.MOUSE_FILTER_IGNORE
