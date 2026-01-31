74f0d56872dc61f6742334baed02a1ac6402c9dd
aitor <aitorizur@gmail.com>
2026-01-31 20:37:29 +0100

    Progress

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,7 +5,8 @@ enum Estados {
 	INICIO,
 	HABLANDO,
 	SIGUIENTE_PJ,
-	MASCAR
+	MASCAR,
+	HABLANDOPOST,
 }
 
 # var enableInput: bool = false
@@ -19,7 +20,9 @@ var estado: int
 @onready var shader = $shader2
 @onready var zonaTexto = $zonaTexto
 
+var mascado : bool
 var currentGrab : objeto_sprite
+var currentPersonajeSprite : Sprite2D
 
 @export var personajes : Array[Personaje]
 @export var nodosPersonaje : Array[Node2D]
@@ -40,6 +43,7 @@ func _input(event: InputEvent) -> void:
 
 func avanzar_juego():
 	if estado == Estados.INICIO:
+		currentPersonajeSprite = get_current_personaje_node().get_node("AnimationPlayer") as Sprite2D
 		get_current_personaje_node().get_node("AnimationPlayer").play("appear")
 		get_current_personaje_node().get_node("./spriteObjeto/AnimationPlayer").play("appear")
 		currentGrab = get_current_personaje_node().find_child("spriteObjeto") as objeto_sprite
@@ -49,35 +53,51 @@ func avanzar_juego():
 			zonaTexto.text = get_current_personaje_data().textoIntro[text_i]
 			text_i += 1
 		else:
+			zonaTexto.text = ""
 			shader.mouse_filter = Control.MOUSE_FILTER_IGNORE
 			currentGrab.OnMouth.connect(OnObjetoArrastradoBoca)
 			currentGrab.OnTrash.connect(OnObjetoArrastradoTrash)
-			
 	elif estado == Estados.SIGUIENTE_PJ:
-		print(personajes.size())
-		print(personajes_i)
 		if personajes_i < personajes.size():
 			get_current_personaje_node().get_node("AnimationPlayer").play("appear")
 			get_current_personaje_node().get_node("./spriteObjeto/AnimationPlayer").play("appear")
 			estado = Estados.HABLANDO
 	elif estado == Estados.MASCAR:
 		mascarMiniGame.next()
+	elif estado == Estados.HABLANDOPOST:
+		if mascado:
+			if text_i < get_current_personaje_data().textoPostMascar.size():
+				zonaTexto.text = get_current_personaje_data().textoPostMascar[text_i]
+				text_i += 1
+			else:
+				GoNextPersonaje()
+		else:
+			if text_i < get_current_personaje_data().textoPostThrash.size():
+				zonaTexto.text = get_current_personaje_data().textoPostThrash[text_i]
+				text_i += 1
+			else:
+				GoNextPersonaje()
 
 func OnMascarEnded():
 	mascarMiniGame.StepsEnded.disconnect(OnMascarEnded)
-	GoNextPersonaje()
+	estado = Estados.HABLANDOPOST
+	mascado = true
+	text_i = 0
 	
 func  OnObjetoArrastradoTrash():
-	currentGrab.visible = false
-	currentGrab.OnTrash.disconnect(OnObjetoArrastradoTrash)
-	currentGrab.OnMouth.disconnect(OnObjetoArrastradoBoca)
-	GoNextPersonaje()
+	OnObjetoArrastrado()
+	mascado = false
+	estado = Estados.HABLANDOPOST
+	text_i = 0
 
 func  OnObjetoArrastradoBoca():
+	OnObjetoArrastrado()
+	GoEstadoMascar()
+
+func OnObjetoArrastrado():
 	currentGrab.visible = false
 	currentGrab.OnTrash.disconnect(OnObjetoArrastradoTrash)
 	currentGrab.OnMouth.disconnect(OnObjetoArrastradoBoca)
-	GoEstadoMascar()
 
 func GoEstadoMascar():
 	mascarMiniGame.Initialize(get_current_personaje_data().mascarData)
@@ -87,8 +107,11 @@ func GoEstadoMascar():
 func GoNextPersonaje():
 	get_current_personaje_node().get_node("AnimationPlayer").play("disappear")
 	if personajes_i < personajes.size():
+		zonaTexto.text = ""
 		personajes_i += 1
 		text_i = 0
 		currentGrab = get_current_personaje_node().find_child("spriteObjeto") as objeto_sprite
+		currentPersonajeSprite = get_current_personaje_node().get_node("AnimationPlayer") as Sprite2D
+		shader.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		estado = Estados.SIGUIENTE_PJ
 	
