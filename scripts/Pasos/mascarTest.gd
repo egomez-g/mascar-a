dbca3e791e65ace97d50687924989d5d5d333d13
egomez-g <edu.gomez.gonzalezz@gmail.com>
2026-02-01 11:18:41 +0100

    game feel

--- scripts/Pasos/mascarTest.gd
+++ scripts/Pasos/mascarTest.gd
@@ -5,6 +5,9 @@ class_name  MascarMinGame
 @export var _mascarArea: Area2D
 @onready var audio_player = $AudioStreamPlayer2D
 var currentIndex: int = 0
+var ini_pos: Vector2
+var fuerzaDelsake: int = 12
+var playSake: bool = false
 var canGoNext: bool = true
 signal StepsEnded
 @export var bocaNeutra: Texture
@@ -15,6 +18,7 @@ func _ready() -> void:
 	StepsEnded.connect(_printStepsEnd)
 	_mascarArea.area_entered.connect(OnAreaEntered)
 	_mascarArea.area_exited.connect(OnAreaExit)
+	ini_pos = position
 
 func _printStepsEnd() -> void:
 	print("Steps ENded")	
@@ -44,6 +48,7 @@ func next() -> void:
 			StepsEnded.emit()
 
 func PlayAnimation() -> void:
+	playSake = true
 	canGoNext = false
 	audio_player.stream = _pasos._pasos[currentIndex].sonido
 	audio_player.play()
@@ -51,3 +56,8 @@ func PlayAnimation() -> void:
 	await  get_tree().create_timer(0.5).timeout
 	texture = _pasos._pasos[currentIndex].sprite2
 	canGoNext = true
+	playSake = false
+
+func _process(delta: float) -> void:
+	if playSake:
+		position = position.lerp(ini_pos + Vector2(randf_range(-fuerzaDelsake, fuerzaDelsake), randf_range(-fuerzaDelsake, fuerzaDelsake)), 0.4)
